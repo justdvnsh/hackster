c2d53e7a33fa322d948ee89f5ef66f0a202b3c96
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-07-17 01:31:48 +0530

    Made some prgress on the scrapin side - scraping the competitoins , download the react-native side of things

--- scraping.py
+++ scraping.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 import pyautogui
 from webdriver_manager.chrome import ChromeDriverManager
+import json
+from flask import Flask
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.maximize_window()
 
-driver.get("https://kaggle.com/competitions")
 
-competitions = driver.find_elements_by_xpath('/html/body/main/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div/div')
-print([competition.text.split('\n') for competition in competitions])
+
+
